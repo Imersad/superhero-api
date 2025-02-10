5b749a91b47f3be3989fda94fcd8ce40a2b5be88
Imersad <imersad94@gmail.com>
2025-02-10 23:26:29 +0100

    Implemet SuperheroesService & addSuperhero property fn

---
+++ backend/src/superheroes/superheroes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Superhero } from '../database/superheroes.interface';
+import { CreateSuperheroDto } from './dto/createSuperhero.dto';
+
+@Injectable()
+export class SuperheroesService {
+  private readonly superheroes: Superhero[] = [];
+
+  addSuperhero(createSuperheroDto: CreateSuperheroDto) {
+    this.superheroes.push(createSuperheroDto);
+
+    return {
+      message: 'Superhero has been created successfully',
+      superheroes: this.superheroes,
+    };
+  }
+}
